2edd2d83d5eb11211cb6446600c1e297c05d4799
DINAMOW <meemoo102039@gmail.com>
2024-04-17 02:15:24 +0200

    update: still not done

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Engine
+
+ENGINE = Engine()
 
 # Create your views here.
+def testing(request):
+    return JsonResponse({'foo': 'bar'}, status=200)
+
+def dates(request):
+    if request.method != 'GET':
+        return JsonResponse(
+            {'error': 'GET request required'},
+            status=400)
+    return JsonResponse(
+        {'dates': ENGINE.get_dates()},
+        status=200)
+
+def balance(request):
+    if request.method != 'GET':
+        return JsonResponse(
+            {'error': 'GET request required'},
+            status=400)
+
+    years = request.GET.get('years')
+    years = years.split(',')
+    if len(years) != 2:
+        return 
+    ratio = request.GET.get('ratio')
+
+    return JsonResponse(
+        {'result': years},
+        status=200)
