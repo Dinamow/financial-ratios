9d069a5c1eaa395abb8100c83753eccb61267a11
DINAMOW <meemoo102039@gmail.com>
2024-04-18 17:53:39 +0200

    add: Ratios, Company, Dates to the admin page

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Ratios, Company, Dates
 # Register your models here.
+
+admin.site.register(Ratios)
+admin.site.register(Company)
+admin.site.register(Dates)
