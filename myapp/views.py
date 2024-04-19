9bb0ebe872bd7747bb8430d52a6cd4214d644bed
DINAMOW <meemoo102039@gmail.com>
2024-04-20 01:01:16 +0200

    improvment: add more error handling

--- myapp/views.py
+++ myapp/views.py
@@ -5,55 +5,7 @@ ENGINE = Engine()
 
 # Create your views here.
 def testing(request):
-    if request.method != 'GET':
-        return JsonResponse(
-            {'error': 'GET request required'},
-            status=400)
-
-    years = request.GET.get('years').split(',')
-    ratio = request.GET.get('ratio')
-    type = request.GET.get('type')
-    company = request.GET.get('company')
-
-    if ratio:
-        data = {
-            "ratio": "current ratio",
-                "formula": "current assets / current liabilities",
-                "componotes": ["current assets", "current liabilities"],
-                "2023": {
-                    "value": 1.5,
-                    "current assets": 1.5,
-                    "current liabilities": 1.0,
-                    "numbers": "1.5 / 1.0"
-                    },
-                "2024": {
-                    "value": 1.5,
-                    "current assets": 1.5,
-                    "current liabilities": 1.0,
-                    "numbers": "1.5 / 1.0"
-                    }
-                }
-        return JsonResponse(data,
-                            status=200)
-
-    if type:
-        data = {
-            "ratios": ["current ratio", "quick ratio"],
-            "2023": {
-                "current ratio": 1.5,
-                "quick ratio": 1.5
-                },
-            "2024": {
-                "current ratio": 1.5,
-                "quick ratio": 1.5
-                }
-            }
-        return JsonResponse(data,
-                            status=200)
-
-    return JsonResponse(
-        {'error': 'Invalid request', 'message': 'Please provide type, company, years'},
-        status=400)
+    return JsonResponse({'foo': 'bar'}, status=200)
 
 def dates(request):
     if request.method != 'GET':
@@ -70,18 +22,28 @@ def balance(request):
             {'error': 'GET request required'},
             status=400)
 
-    years = request.GET.get('years').split(',')
+    years = request.GET.get('years')
     ratio = request.GET.get('ratio')
     type = request.GET.get('type')
     company = request.GET.get('company')
+
+    if not years:
+        return JsonResponse(
+            {'error': 'Missing years'},
+            status=400)
+
+    years = years.split(',')
 
     if ratio:
         return JsonResponse(ENGINE.get_ratio(ratio, years, company),
                             status=200)
 
     if type:
-        return JsonResponse(ENGINE.get_type(type, years, company),
-                            status=200)
+        resp = ENGINE.get_type(type, years, company)
+        if 'error' in resp.keys():
+            return JsonResponse(resp, status=400)
+        return JsonResponse(resp, status=200)
+            
 
     return JsonResponse(
         {'error': 'Invalid request', 'message': 'Please provide type, company, years'},
