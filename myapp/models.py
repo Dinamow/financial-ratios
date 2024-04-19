123b0653294549d047314fba97811ecc671c48fb
Mohamed Ibrahim <mibrahimm7010@gmail.com>
2024-04-19 17:48:35 +0200

    fixed 'sqrt' related bug in get_components method

--- myapp/models.py
+++ myapp/models.py
@@ -509,6 +509,7 @@ class Engine(Lequidity, AssetsTO, Profitability, MarketValue):
         }
 
     def get_components(self, formula):
+        str(formula).replace("sqrt", "")
         components = re.findall(r'\b\w+\b', str(formula))
         return components
 
