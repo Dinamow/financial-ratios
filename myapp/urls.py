c13a3e916714bcf256875aabbadd119c082c5fe8
DINAMOW <meemoo102039@gmail.com>
2024-04-24 23:42:20 +0200

    fix: remove the slashes cuz django handle them auto

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('', landing, name='landing'),
     path('one', one_year, name='one_year'),
     path('Compare', compare, name='compare'),
-    path('dates/', dates, name='dates'),
-    path('balance/', balance, name='balance'),
-    path('create/', create, name='create'),
-    path('save/', save, name='save')
+    path('dates', dates, name='dates'),
+    path('balance', balance, name='balance'),
+    path('create', create, name='create'),
+    path('save', save, name='save')
 ]
